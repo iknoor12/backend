eb71f900ea7bffa27497bdf0161815a0859c3449
iknoor12 <imiknoor@gmail.com>
2026-03-28 13:47:53 +0530

    day-3 handling routes

---
+++ day-3-handleRoutes/server.js
@@ -0,0 +1,19 @@
+const http =require('http');
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/'){
+        res.write("Welcome to Home Page 🏠");
+    } else if(req.url === '/about'){
+        res.write("Welcome to About Page 📄");
+    } else if(req.url === '/contact'){
+        res.write("Welcome to Contact us at contact@example.com 📞 page");
+    } else {
+        res.write("404 Page not found!");
+    }
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
